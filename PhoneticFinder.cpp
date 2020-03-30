08699f68591203abc20b3ec8281ca80bbe30a684
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-03-30 22:03:59 +0300

    Update PhoneticFinder.cpp

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -4,62 +4,59 @@
 #include <cstring>
 #include <ctype.h>
 #include<bits/stdc++.h> 
+#include <stdexcept>
+#include <exception>
 using namespace std;
 
 namespace phonetic {
 
-    int same(char a, char b) {
-        if ((a == 'v' && b == 'w') || (b == 'v' && a == 'w'))return 0;
-        if ((a == 'g' && b == 'j') || (b == 'g' && a == 'j'))return 0;
-        if ((a == 's' && b == 'z') || (b == 's' && a == 'z'))return 0;
-        if ((a == 't' && b == 'd') || (b == 't' && a == 'd'))return 0;
-        if ((a == 'o' && b == 'u') || (b == 'o' && a == 'u'))return 0;
-        if ((a == 'y' && b == 'i') || (b == 'y' && a == 'i'))return 0;
-        if ((a == 'b' && b == 'f') || (b == 'b' && a == 'f') || (a == 'b' && b == 'p') || (b == 'b' && a == 'p'))
-            return 0;
-        if ((a == 'f' && b == 'p') || (b == 'f' && a == 'p'))return 0;
-        if ((a == 'c' && b == 'q') || (b == 'c' && a == 'q') || (a == 'q' && b == 'k') || (b == 'q' && a == 'k'))
-            return 0;
-        if ((a == 'c' && b == 'k') || (b == 'c' && a == 'k'))return 0;
-        return 1;
-    }
+	int same(char a, char b) {
+		if ((a == 'v' && b == 'w') || (b == 'v' && a == 'w'))return 0;
+		if ((a == 'g' && b == 'j') || (b == 'g' && a == 'j'))return 0;
+		if ((a == 's' && b == 'z') || (b == 's' && a == 'z'))return 0;
+		if ((a == 't' && b == 'd') || (b == 't' && a == 'd'))return 0;
+		if ((a == 'o' && b == 'u') || (b == 'o' && a == 'u'))return 0;
+		if ((a == 'y' && b == 'i') || (b == 'y' && a == 'i'))return 0;
+		if ((a == 'b' && b == 'f') || (b == 'b' && a == 'f') || (a == 'b' && b == 'p') || (b == 'b' && a == 'p'))
+			return 0;
+		if ((a == 'f' && b == 'p') || (b == 'f' && a == 'p'))return 0;
+		if ((a == 'c' && b == 'q') || (b == 'c' && a == 'q') || (a == 'q' && b == 'k') || (b == 'q' && a == 'k'))
+			return 0;
+		if ((a == 'c' && b == 'k') || (b == 'c' && a == 'k'))return 0;
+		return 1;
+	}
+
+	string find(string a, string b) {
+
+		int count = 0;
+
+		string word;
+		// making a string stream 
+		stringstream iss(a);
+
+		while (iss >> word) {
+			if (word.length() != b.length())break;
+			if (ans == "") {
+				throw std::runtime_error(std::string("Exception"));
+			}
+			int count = 0;//count the correct chars
+			for (int i = 0; i < word.length(); i++) {//run on chars
+				if (same((char)tolower(word[i]), (char)tolower(b[i])) == 0 || (char)tolower(word[i]) == (char)tolower(b[i]))
+				{
+					count++;
+				}
+				if (word == "")return word = "Exception";;
+				//if the same length to the 2 words return the answer!!!
+				if ((count == b.length() && count == word.length())) {
+					return word;
+				}
+
+			}
+			count = 0;//reset counter for next word
+		}
+
+		return word = "Exception";;
 
 
-    string find(string a, string b) {
-//  if std::strlen(a)!=std::strlen(b)return null;   irrelevant because obviously lengths are diffrent
-
- int count = 0;
-
- string word; 
-    // making a string stream 
-    stringstream iss(a); 
-
- // try {
-    while (iss >> word) {
-            int count = 0;//count the correct chars
-            for (int i = 0; i < word.length(); i++) {//run on chars
-                if (same((char)tolower(word[i]), (char)tolower(b[i])) == 0||(char)tolower(word[i])== (char)tolower(b[i]))
-{
-count++;
-}
- if(word=="")return word="Exception";;
-//if the same length to the 2 words return the answer!!!
- if ( (count == b.length() && count== word.length())) {
-                    return word;
-                } 
-
-}
-count=0;//reset counter for next word
-           }
-//        } catch (int e) {
-//            cout << "Exception 10: Not the same word";
-//      return word="Exception";
-//        }
-               
-    
-
-        return word="Exception";;
-
-
-    }
+	}
 }
