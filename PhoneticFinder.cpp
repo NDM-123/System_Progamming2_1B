d74557b6ee835991b60a091fe8ad26b6262daeb6
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-03-31 10:56:34 +0300

    Update PhoneticFinder.cpp

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 namespace phonetic {
 
+	/*check problematic chars that can sound the same return 0 if its the same*/
 	int same(char a, char b) {
 		if ((a == 'v' && b == 'w') || (b == 'v' && a == 'w'))return 0;
 		if ((a == 'g' && b == 'j') || (b == 'g' && a == 'j'))return 0;
@@ -25,38 +26,39 @@ namespace phonetic {
 		if ((a == 'c' && b == 'k') || (b == 'c' && a == 'k'))return 0;
 		return 1;
 	}
-
+/*the function to search our word in the text ,a is the text and b is the word */
 	string find(string a, string b) {
 
 		int count = 0;
 
 		string word;
-		// making a string stream 
+		/* making a string stream*/ 
 		stringstream iss(a);
 
 		while (iss >> word) {
 			if (word.length() != b.length()) continue;	//if the stream is per word
 			
-			int count = 0;//count the correct chars
+			int count = 0;	//count the correct chars
 			
-			for (int i = 0; i < word.length(); i++) {//run on chars
+			for (int i = 0; i < word.length(); i++) {	//run on chars
 				if (same((char)tolower(word[i]), (char)tolower(b[i])) == 0 || (char)tolower(word[i]) == (char)tolower(b[i]))
 				{
 					count++;
-				}//else{throw std::runtime_error(std::string("Exception"));}
-				if (word == "")	throw std::runtime_error(std::string("Exception - Word wasnt found"));
-				//if the same length to the 2 words return the answer!!!
-				if ((count == b.length() && count == word.length())) {// && b.length()==word.length())) {
+				}
+	/*throw exception if an empty word*/			
+				if (word == "")	throw std::runtime_error(std::string("Exception - Empty word"));
+	/*if the same length to the word in the text and the word we are searching return the answer*/
+				if ((count == b.length() && count == word.length())) {
 					return word;
 				}
 
 			}
-			count = 0;//reset counter for next word
+			count = 0;	//reset counter for next word
 		}
-	//	if (word == "") {
+	/*throw exception if word wasnt found*/
 				throw std::runtime_error(std::string("Exception - Word wasnt found"));
-	//		}
-		return word = "Exception";
+
+		return word;
 
 
 	}
